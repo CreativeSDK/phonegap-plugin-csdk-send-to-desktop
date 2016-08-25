746991c6eaa00c0cb31b0f2654219dc3c05894de
Ash <ash@ashryanbeats.com>
2016-08-25 13:55:55 -0400

    First pass at JSDoc.

--- www/SendToDesktop.js
+++ www/SendToDesktop.js
@@ -25,7 +25,21 @@
 var exec = cordova.require('cordova/exec'),
     utils = cordova.require('cordova/utils');
 
+/**
+    @description A global object that lets you interact with the Creative SDK Send To Desktop API.
+    @global
+*/
 var CSDKSendToDesktop = {
+    /**
+     * @description sends an image to an Adobe desktop app.
+     * @function send
+     * @memberof CSDKSendToDesktop
+     * @param {!successCallback} successCallback - See type definition.
+     * @param {!failureCallback} failureCallback - See type definition.
+     * @param {!string} uri URI of the image to be sent.
+     * @param {!CSDKSendToDesktop.AppType} ccApplication The Adobe Creative Cloud desktop application you are sending to.
+     * @param {!string} mimeType The mime type of the image you are sending.
+     */
     send: function(successCallback, failureCallback, uri, ccApplication, mimeType) {
         exec(successCallback, failureCallback, 'CSDKSendToDesktop', 'send', [uri, ccApplication, mimeType]);
     },
@@ -33,11 +47,27 @@ var CSDKSendToDesktop = {
     /**
     * @enum {number}
     */
-   AppType:{
-       PHOTOSHOP: 0,
-       INDESIGN: 1,
-       ILLUSTRATOR: 2
-   }
+    AppType:{
+        /** Send to Photoshop */
+        PHOTOSHOP: 0,
+        /** Send to InDesign */
+        INDESIGN: 1,
+        /** Send to Illustrator */
+        ILLUSTRATOR: 2
+    }
 };
+
+/**
+ * @description A callback to be used upon sending of an image. This callback has no parameters.
+ *
+ * @callback successCallback
+ */
+
+/**
+ * @description A callback to handle errors when attempting to send an image.
+ *
+ * @callback failureCallback
+ * @param {Object} error - Error object.
+ */
 
 module.exports = CSDKSendToDesktop;
